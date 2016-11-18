20d98a98bc7e302e55416b4f024aa0d95384b771
nullnorth <todd.lewis606@gmail.com>
2016-11-18 15:22:52 -0700

    Create Variable Tracking
    Made a temp file for following up on variables that are populated
    statically

---
+++ phase1-pre-server-role-configuration/set-computer-name.ps1
@@ -0,0 +1,6 @@
+﻿#variables
+#$domainname
+#$provisionname
+#$domainadmincred
+
+Add-Computer -ComputerName $env:COMPUTERNAME -Domain $domainname -NewName $provisionname -Credential $domainadmincred -force -Restart
