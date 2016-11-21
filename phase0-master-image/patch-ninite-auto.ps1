f088e1ae1912e113763c45d5993c0d572625ee09
nullnorth <todd.lewis606@gmail.com>
2016-11-21 16:16:04 -0700

    added wait until feature
    made remove file AFTER the ninite app has closed.

--- phase0-master-image/patch-ninite-auto.ps1
+++ phase0-master-image/patch-ninite-auto.ps1
@@ -1,6 +1,5 @@
 ﻿$outlocation = "C:\temp-ninite.exe"
 #$niniteURL
 invoke-webrequest -uri "https://ninite.com/eclipse/ninite.exe" -OutFile $outlocation
-&$outlocation /silent
-Start-Sleep -Seconds 600
+start-process $outlocation -NoNewWindow -Wait
 Remove-Item -Path $outlocation
