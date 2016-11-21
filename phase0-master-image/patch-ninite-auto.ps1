cffeda911d73aebb45df72cfed2c21349923b740
nullnorth <todd.lewis606@gmail.com>
2016-11-21 15:11:23 -0700

    generation
    Basic hacky powershell call against ninite.com

---
+++ phase0-master-image/patch-ninite-auto.ps1
@@ -0,0 +1,6 @@
+﻿$outlocation = "C:\temp-ninite.exe"
+#$niniteURL
+invoke-webrequest -uri "https://ninite.com/eclipse/ninite.exe" -OutFile $outlocation
+&$outlocation /silent
+Start-Sleep -Seconds 600
+Remove-Item -Path $outlocation
