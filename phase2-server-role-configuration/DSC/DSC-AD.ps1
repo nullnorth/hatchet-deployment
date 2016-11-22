5e12bcbeec34b5627a0e60dd16c8eefed7543beb
nullnorth <todd.lewis606@gmail.com>
2016-11-22 16:07:17 -0700

    created base domain files

---
+++ phase2-server-role-configuration/DSC/DSC-AD.ps1
@@ -0,0 +1,67 @@
+﻿configuration DSC-AD             
+{             
+   param             
+    (             
+        [Parameter(Mandatory)]             
+        [pscredential]$safemodeAdministratorCred,             
+        [Parameter(Mandatory)]            
+        [pscredential]$domainCred            
+    )             
+            
+    Import-DscResource -ModuleName xActiveDirectory             
+            
+    Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename             
+    {             
+            
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        }            
+            
+        File ADFiles            
+        {            
+            DestinationPath = 'C:\NTDS'            
+            Type = 'Directory'            
+            Ensure = 'Present'            
+        }            
+                    
+        WindowsFeature ADDSInstall             
+        {             
+            Ensure = "Present"             
+            Name = "AD-Domain-Services"             
+        }            
+            
+        # Optional GUI tools            
+        WindowsFeature ADDSTools            
+        {             
+            Ensure = "Present"             
+            Name = "RSAT-ADDS"             
+        }            
+            
+        # No slash at end of folder paths            
+        xADDomain FirstDS             
+        {             
+            DomainName = $Node.DomainName             
+            DomainAdministratorCredential = $domainCred             
+            SafemodeAdministratorPassword = $safemodeAdministratorCred            
+            DatabasePath = 'N:\NTDS'            
+            LogPath = 'N:\NTDS'            
+            DependsOn = "[WindowsFeature]ADDSInstall","[File]ADFiles"            
+        }            
+            
+    }             
+}                     
+
+DSC-AD -ConfigurationData $ConfigData `
+    -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' `
+        -Message "New Domain Safe Mode Administrator Password") `
+    -domainCred (Get-Credential -UserName alpineskihouse\administrator `
+        -Message "New Domain Admin Credential")            
+            
+# Make sure that LCM is set to continue configuration after reboot            
+Set-DSCLocalConfigurationManager -Path .\DSC-AD –Verbose            
+            
+# Build the domain            
+Start-DscConfiguration -Wait -Force -Path .\DSC-AD -Verbose            
---
+++ phase2-server-role-configuration/DSC/DSC-AD.psd1
@@ -0,0 +1,13 @@
+﻿# Configuration Data for AD              
+$ConfigData = @{             
+    AllNodes = @(             
+        @{             
+            Nodename = "localhost"             
+            Role = "Primary DC"             
+            DomainName = "alpineskihouse.com"             
+            RetryCount = 20              
+            RetryIntervalSec = 30            
+            PsDscAllowPlainTextPassword = $true            
+        }            
+    )             
+}   
