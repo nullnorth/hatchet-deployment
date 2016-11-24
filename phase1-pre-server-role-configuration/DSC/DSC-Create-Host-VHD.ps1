9b0951d182bd68271b98f30486b1eb359073731f
nullnorth <todd.lewis606@gmail.com>
2016-11-24 12:54:27 -0700

    Document DSC equivalent of scripts currently available.
    Created DSC scripts from samples
    Documented which combination of modules needed to achieve taks.

---
+++ phase1-pre-server-role-configuration/DSC/DSC-Create-Host-VHD.ps1
@@ -0,0 +1,34 @@
+﻿Configuration Sample_xVHD_NewVHD
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]$Name,
+
+        [Parameter(Mandatory)]
+        [string]$Path,
+
+        [Parameter(Mandatory)]
+        [Uint64]$MaximumSizeBytes,
+
+        [ValidateSet("Vhd","Vhdx")]
+        [string]$Generation = "Vhd",
+
+        [ValidateSet("Present","Absent")]
+        [string]$Ensure = "Present"        
+    )
+
+    Import-DscResource -module xHyper-V
+
+    Node localhost
+    {
+        xVHD NewVHD
+        {
+            Ensure           = $Ensure
+            Name             = $Name
+            Path             = $Path
+            Generation       = $Generation
+            MaximumSizeBytes = $MaximumSizeBytes
+        }
+    }
+}
---
+++ phase1-pre-server-role-configuration/DSC/DSC-EnableRDP.ps1
@@ -0,0 +1,34 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingComputerNameHardcoded', '')] 
+param()
+
+Configuration AllowRemoteDesktopAdminConnections
+{
+    Import-DscResource -Module xRemoteDesktopAdmin, xNetworking
+
+    Node ('localhost')
+    {        
+        xRemoteDesktopAdmin RemoteDesktopSettings
+        {
+           Ensure = 'Present'
+           UserAuthentication = 'NonSecure'
+        }
+
+        xFirewall AllowRDP
+        {
+            Name = 'DSC - Remote Desktop Admin Connections'
+            DisplayGroup = "Remote Desktop"
+            Ensure = 'Present'
+            State = 'Enabled'
+            Access = 'Allow'
+            Profile = 'Domain'
+        }
+    }
+}
+
+$workingdir = 'C:\RDP\MOF'
+
+# Create MOF
+AllowRemoteDesktopAdminConnections -OutputPath $workingdir
+
+# Apply MOF
+Start-DscConfiguration -ComputerName 'localhost' -wait -force -verbose -path $workingdir
