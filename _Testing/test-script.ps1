3f8ec58522c7c430edfb6db92b275c7db05d71ce
nullnorth <todd.lewis606@gmail.com>
2016-12-06 13:22:18 -0700

    Test of script DSC Resource
    First commit

---
+++ _Testing/test-script.ps1
@@ -0,0 +1,27 @@
+﻿configuration DSC-test-script 
+{
+
+    Node $AllNodes.Where{$_.Role -eq "Primary DC"}.Nodename
+    {
+    LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true
+        }
+        
+    script Scripttest
+        {
+            GetScript = 
+            {
+                $env:COMPUTERNAME
+            }
+            TestScript = 
+            {
+            }
+            SetScript = 
+            {
+            }    
+        }         
+    }
+}
