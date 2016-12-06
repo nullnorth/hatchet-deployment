38829debbf419b05134f92acf0e0bb26cc66cd3c
nullnorth <todd.lewis606@gmail.com>
2016-12-06 13:21:46 -0700

    Created-rename-computer

--- phase1-pre-server-role-configuration/set-computer-name.ps1
+++ phase1-pre-server-role-configuration/set-computer-name.ps1
@@ -1,6 +1,1 @@
-﻿#variables
-#$domainname
-#$provisionname
-#$domainadmincred
-
-Add-Computer -ComputerName $env:COMPUTERNAME -Domain $domainname -NewName $provisionname -Credential $domainadmincred -force -Restart
+﻿Rename-Computer -ComputerName $ComputerName -NewName $ProvisionName -restart -Force
